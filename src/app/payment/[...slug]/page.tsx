43991739820967655ff1bdeed6f8f40fed866b43
iqmalr <m.iqmal.riffai@gmail.com>
2024-11-03 00:05:30 +0700

    repair slug type 2

--- src/app/payment/[...slug]/page.tsx
+++ src/app/payment/[...slug]/page.tsx
@@ -1,27 +1,37 @@
 import PaymentPage from "@/components/pages/payment-page";
 import services from "@/data/services";
-
-type Params = {
-  slug: string[];
-};
+import { Metadata } from "next";
 
 type Props = {
-  params: Params;
+  params: {
+    slug: string[];
+  };
+  searchParams: { [key: string]: string | string[] | undefined };
 };
 
-const Payment = async ({ params }: Props) => {
+export const generateMetadata = async ({
+  params,
+}: {
+  params: { slug: string[] };
+}): Promise<Metadata> => {
+  return {
+    title: "Payment - SIMS PPOB",
+  };
+};
+
+export default function Payment({ params, searchParams }: Props) {
   const { slug } = params;
+
   if (!slug || slug.length === 0) {
     return null;
   }
-  const [serviceType] = slug;
-  const service = services.find(
-    (s) => s.service_code.toLowerCase() === serviceType
-  );
+
+  const serviceType = slug[0].toUpperCase();
+  const service = services.find((s) => s.service_code === serviceType);
+
   if (!service) {
     return <p>Service not found</p>;
   }
-  return <PaymentPage serviceType={service?.service_name} />;
-};
 
-export default Payment;
+  return <PaymentPage serviceType={service.service_name} />;
+}
